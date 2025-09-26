a3456d311b87d49aa5f3fe68458d0dd1ef54a317
ProfWHendrickson <whendrickson@lewisu.edu>
2025-09-26 08:24:53 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
+function greet(name = "World") {
+  return `Hello, ${name}!`;
+}
+
+function verifyAccount() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("passwd").value.toLowerCase();
+	if (username == "ray" && password == "pizza") {
+		alert("Congratulations. You are logged in.");
+	} else {
+		alert("Boo. Hiss. You are hacker. Go away.");
+	}
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function square(x) {
+  return x * x;
+}
+
+let count = 0;
+function IncrementCount()
+{
+      // Start with a number
+    let count = 0;
+
+    // Get references to DOM elements
+    const countDisplay = document.getElementById("count");
+    const button = document.getElementById("incrementBtn");
+
+    // Add a click event
+    button.addEventListener("click", () => {
+      count++; // increase by 1
+      countDisplay.textContent = count; // update the display
+    });
+}
 
